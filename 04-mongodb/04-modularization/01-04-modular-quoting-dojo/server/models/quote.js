0bd79c5f61992d3ab687aba0c9e22b30e509924d
Remote Storage <julie.tenebih@gmail.com>
2019-04-18 22:44:10 -0400

    assignments done

---
+++ 04-mongodb/04-modularization/01-04-modular-quoting-dojo/server/models/quote.js
@@ -0,0 +1,10 @@
+var mongoose = require('../config/mongoose');
+const { Schema } = mongoose;
+
+var QuoteSchema = new mongoose.Schema({
+    name: String,
+    quote: String
+}, {timestamps: true});
+mongoose.model('Quote', QuoteSchema);
+
+module.exports = mongoose.model('Quote', QuoteSchema);
