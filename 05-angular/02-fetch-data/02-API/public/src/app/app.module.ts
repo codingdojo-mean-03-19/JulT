46ded3196b971a581be0ff8e963e123b23e60439
Remote Storage <julie.tenebih@gmail.com>
2019-04-20 01:07:59 -0400

    Assignments done

--- 05-angular/02-fetch-data/02-API/public/src/app/app.module.ts
+++ 05-angular/02-fetch-data/02-API/public/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HttpService } from './http.service';
 import { HttpClientModule } from '@angular/common/http';
-
 
 @NgModule({
   declarations: [
@@ -13,7 +13,8 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [HttpService],
   bootstrap: [AppComponent]
